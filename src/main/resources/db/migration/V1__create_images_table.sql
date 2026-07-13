c7d2e8f604d6d4b9ac051e9275a4c7ebbf5cf53f
TelinaTsantaFitia <hei.telina@gmail.com>
2026-07-13 08:53:51 +0300

    feat: add dependancies

---
+++ src/main/resources/db/migration/V1__create_images_table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE images (
+    id UUID PRIMARY KEY,
+    mail VARCHAR(255) NOT NULL,
+    nom_fichier VARCHAR(255) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    s3_key VARCHAR(255),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
